2b074048c55afa0bd2f6edd93de8cc8895cd65ce
chetan578 <sobti.chetan.2009@gmail.com>
2021-05-09 22:51:15 +0530

    React app initial changes

---
+++ src/frontend/src/components/MatchDetailCard.js
@@ -0,0 +1,13 @@
+import {React} from 'react'
+
+export const MatchDetailCard=({match})=>{
+    if(match==null)
+    return null;
+    return(
+        <div className="MatchDetailCard">
+        <h3>Latest Matches</h3>
+        <h4>Match details</h4>
+       <h4> {match.team1} vs {match.team2} </h4>
+        </div>
+    )
+}
